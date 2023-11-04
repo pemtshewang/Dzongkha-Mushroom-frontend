089898b7bbcf7762459074da96a59f17b894af1e
pemtshewang <pemtshewang500@gmail.com>
2023-11-04 13:34:04 +0600

    changed

--- app/detector/page.tsx
+++ app/detector/page.tsx
@@ -35,9 +35,8 @@ export default function Page() {
       body: formData,
       cache: "no-store",
     })
-    const response = await res.json();
-    alert(response.prediction.capitalize());
-    setEnglishName(response.prediction.capitalize());
+    const name = await res.json();
+    setEnglishName(name.prediction);
     setLoading(false);
     getMushroomName();
   }
